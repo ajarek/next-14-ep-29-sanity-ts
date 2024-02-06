afb40c126c662b4d426f6a6f4126841b8ca7a693
Ajarek <ajarek@poczta.onet.pl>
2024-02-06 22:11:52 +0100

    added logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 // ./app/page.tsx
 
+// ./app/page.tsx
+
 import { SanityDocument } from "next-sanity";
+import { draftMode } from "next/headers";
 
 import Posts from "@/components/Posts";
+import PostsPreview from "@/components/PostPreview";
 import { loadQuery } from "@/sanity/lib/store";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Page() {
-  const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY);
+  const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY, {}, {
+    perspective: draftMode().isEnabled ? "previewDrafts" : "published",
+  });
 
-  return <Posts posts={initial.data} />
+  return draftMode().isEnabled ? (
+    <PostsPreview initial={initial} />
+  ) : (
+    <Posts posts={initial.data} />
+  )
 }
--- sanity.config.ts
+++ sanity.config.ts
@@ -5,11 +5,11 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
+import { presentationTool } from 'sanity/presentation'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schema'
-
+import { locate } from '@/sanity/presentation/locate'
 export default defineConfig({
   basePath: '/studio',
   projectId,
@@ -21,5 +21,13 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    presentationTool({
+      locate,
+      previewUrl: {
+        draftMode: {
+          enable: '/api/draft',
+        },
+      },
+    }),
   ],
 })
